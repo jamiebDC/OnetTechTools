156a4741e5414b95349c347f324c19a22b51e04b
jamiebDC <jamiebuss@gmail.com>
2021-10-27 09:24:32 -0400

    refined analysis

--- HotTech.py
+++ HotTech.py
@@ -49,35 +49,3 @@ SameUpper = df805upperunique.loc[ df805upperunique.Example.isin(df790upperunique
 # The data is inconsistent. 
 # "SharePoint" vs "Sharepoint"
 # "Microstation" vs "MicroStation"
-
-## How many of the Technology Skills examples are in ERP, or CAD, or neither?
-df = tsh.copy()
-df.rename(columns={
-    "Commodity Code" :  "CCode",
-    "Commodity Title":  "CTitle",
-    "Hot Techology":    "HotTech"}, inplace=True)
-
-# How many erp, unique?  Answer:  275 
-erp = df.loc[ df.CCode == 43231602 ]
-erpu = erp.Example.unique()
-
-# How many CAD, unique?  Answer:  355 
-cad = df.loc[ df.CCode == 43232604 ]
-cadu = cad.Example.unique()
-
-#How many neither CAD nor ERP, unique?  
-neither = df.loc[ ~ df.CCode.isin([43231602,43232604])]
-neitheru = neither.Example.unique()
-# Answer: 181. Expected 175, because 805 = 275+355+175
-
-#How many unique Cmomodity codes altogether? 51
-CommUniq = tsh.CCode.unique()
-CommUniq.shape # 51
-#How many unique Commodity Codes in each group? 
-#How many unique Commodity Codes in the erp group? Answer:  1
-erpCcodes = erp.CCode.value_counts()
-#How many unique Commodity Codes in the cad group? Answer:  1
-cadCcodes = cad.CCode.value_counts()
-#How many unique Commodity Codes in the "neither" group? Answer:  49
-neithercodes = neither.CCode.value_counts()
-# Conclusion:  51 unique commodity codes in all. 
---
+++ HotTech2.py
@@ -0,0 +1,41 @@
+import pandas as pd
+ts = pd.read_excel(r'Technology Skills.xlsx')
+ts.rename(columns={"O*NET-SOC Code": "Onetcode",
+    "Commodity Code" :  "CCode",
+    "Commodity Title":  "CTitle",
+    "Hot Techology":    "HotTech"}, inplace=True)
+
+# hot techs only; where the column HotTech = "Y"
+tsh = ts.loc[ (ts.iloc[:,5] == 'Y')]
+
+## How many of the Technology Skills examples are in ERP, or CAD, or neither?
+df = tsh.copy()
+df.rename(columns={
+    "Commodity Code" :  "CCode",
+    "Commodity Title":  "CTitle",
+    "Hot Techology":    "HotTech"}, inplace=True)
+
+# How many erp, unique?  Answer:  275 
+erp = df.loc[ df.CCode == 43231602 ]
+erpu = erp.Example.unique()
+
+# How many CAD, unique?  Answer:  355 
+cad = df.loc[ df.CCode == 43232604 ]
+cadu = cad.Example.unique()
+
+#How many neither CAD nor ERP, unique?  
+neither = df.loc[ ~ df.CCode.isin([43231602,43232604])]
+neitheru = neither.Example.unique()
+# Answer: 181. Expected 175, because 805 = 275+355+175
+
+#How many unique Cmomodity codes altogether? 51
+CommUniq = tsh.CCode.unique()
+CommUniq.shape # 51
+#How many unique Commodity Codes in each group? 
+#How many unique Commodity Codes in the erp group? Answer:  1
+erpCcodes = erp.CCode.value_counts()
+#How many unique Commodity Codes in the cad group? Answer:  1
+cadCcodes = cad.CCode.value_counts()
+#How many unique Commodity Codes in the "neither" group? Answer:  49
+neithercodes = neither.CCode.value_counts()
+# Conclusion:  51 unique commodity codes in all. 
